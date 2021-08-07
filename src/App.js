c38028455cf0e74b1bc90d9d6f0198048ad78311
Guilherme Fabrin Franco <guilherme.franco@hp.com>
2021-08-07 17:31:27 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.scss';
-import './i18n';
-import { Container, Row, Col, Navbar, Nav, NavDropdown, Badge } from 'react-bootstrap';
-import { faBell, faGlobeAmericas, faCompactDisc } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { NumberFormat } from './formatters/NumberFormat';
-import { ReadyState } from 'react-use-websocket';
-import { useState } from 'react';
-import { withNamespaces } from 'react-i18next';
-import logo from './assets/images/icon.png'
-import WebSocketComponent from './components/WebSocketComponent';
+import "./App.scss";
+import "./i18n";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Badge,
+} from "react-bootstrap";
+import {
+  faBell,
+  faGlobeAmericas,
+  faCompactDisc,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NumberFormat } from "./formatters/NumberFormat";
+import { ReadyState } from "react-use-websocket";
+import { useState } from "react";
+import { withTranslation } from "react-i18next";
+import logo from "./assets/images/icon.png";
+import WebSocketComponent from "./components/WebSocketComponent";
 
-let lastReadyState = null
+let lastReadyState = null;
 
 const themes = [
   "cerulean",
@@ -38,128 +50,171 @@ const themes = [
   "vapor",
   "yeti",
   "zephyr",
-]
+];
 
 function App({ t }) {
-
-  const [notificationsTransactions, setNotificationsTransactions] = useState('')
-  const [notificationsInvoices, setNotificationsInvoices] = useState('')
-  const [notificationsCaptcha, setNotificationsCaptcha] = useState('')
-  const [notificationCount, setNotificationCount] = useState(0)
-  const [stateConnection, setStateConnection] = useState('')
-  const [user, setUserMain] = useState({})
-  const [compactMode, setCompactMode] = useState(1)
-  const [fontSize, setFontSize] = useState(1)
-  const [theme, setTheme] = useState('united')
+  const [notificationsTransactions, setNotificationsTransactions] =
+    useState("");
+  const [notificationsInvoices, setNotificationsInvoices] = useState("");
+  const [notificationsCaptcha, setNotificationsCaptcha] = useState("");
+  const [notificationCount, setNotificationCount] = useState(0);
+  const [stateConnection, setStateConnection] = useState("");
+  const [user, setUserMain] = useState({});
+  const [compactMode, setCompactMode] = useState(1);
+  const [fontSize, setFontSize] = useState(1);
+  const [theme, setTheme] = useState("united");
 
   function confirmCaptcha(id) {
-    const value = prompt('Who is this captcha?')
+    const value = prompt("Who is this captcha?");
     if (value) {
-      WebSocketComponent.setCaptcha(id, value)
+      WebSocketComponent.setCaptcha(id, value);
     }
   }
   function setSeen(notification) {
-    WebSocketComponent.setSeen(notification.id)
+    WebSocketComponent.setSeen(notification.id);
   }
   function toogleCompactMode() {
-    const newCompactMode = compactMode ? 0 : 1
-    WebSocketComponent.setConfig(4, newCompactMode)
-    setCompactMode(newCompactMode)
+    const newCompactMode = compactMode ? 0 : 1;
+    WebSocketComponent.setConfig(4, newCompactMode);
+    setCompactMode(newCompactMode);
   }
   function toogleFontSize(value) {
-    const newFontSize = fontSize * value
-    WebSocketComponent.setConfig(3, newFontSize)
-    setFontSize(newFontSize)
+    const newFontSize = fontSize * value;
+    WebSocketComponent.setConfig(3, newFontSize);
+    setFontSize(newFontSize);
   }
   function toogleTheme(value) {
-    WebSocketComponent.setConfig(2, value)
-    setTheme(value)
+    WebSocketComponent.setConfig(2, value);
+    setTheme(value);
   }
 
   const setters = {
     user: (main_user) => {
-      setUserMain(main_user)
-      setCompactMode(getConfig(4, 0, main_user))
-      setFontSize(getConfig(3, 1, main_user))
-      setTheme(getConfig(2, 'united', main_user))
+      setUserMain(main_user);
+      setCompactMode(getConfig(4, 0, main_user));
+      setFontSize(getConfig(3, 1, main_user));
+      setTheme(getConfig(2, "united", main_user));
     },
     readyState: (readyState) => {
       if (lastReadyState === readyState) {
-        return
+        return;
       }
-      lastReadyState = readyState
+      lastReadyState = readyState;
       switch (readyState) {
         case ReadyState.CONNECTING:
-          setStateConnection(<font className="text-warning"><FontAwesomeIcon icon={faGlobeAmericas} /></font>)
+          setStateConnection(
+            <font className="text-warning">
+              <FontAwesomeIcon icon={faGlobeAmericas} />
+            </font>
+          );
           break;
         case ReadyState.OPEN:
-          setStateConnection(<font className="text-success"><FontAwesomeIcon icon={faGlobeAmericas} /></font>)
+          setStateConnection(
+            <font className="text-success">
+              <FontAwesomeIcon icon={faGlobeAmericas} />
+            </font>
+          );
           break;
         default:
-          setStateConnection(<font className="text-danger"><FontAwesomeIcon icon={faGlobeAmericas} /></font>)
+          setStateConnection(
+            <font className="text-danger">
+              <FontAwesomeIcon icon={faGlobeAmericas} />
+            </font>
+          );
           break;
       }
     },
     notifications: ({ transactions, invoices, captchas }) => {
-      setNotificationCount(transactions.length + invoices.length + captchas.length)
-      const lNotificationsTransactions = []
+      setNotificationCount(
+        transactions.length + invoices.length + captchas.length
+      );
+      const lNotificationsTransactions = [];
       for (const { notification, transaction } of transactions) {
         lNotificationsTransactions.push(
-          <NavDropdown.Item key={`notification_transaction_${transaction.id}`} onClick={() => setSeen(notification)}>
-            {t('common.imported')} {transaction.description}
-            <small> <NumberFormat t={t} value={transaction.value} /></small>
+          <NavDropdown.Item
+            key={`notification_transaction_${transaction.id}`}
+            onClick={() => setSeen(notification)}
+          >
+            {t("common.imported")} {transaction.description}
+            <small>
+              {" "}
+              <NumberFormat t={t} value={transaction.value} />
+            </small>
           </NavDropdown.Item>
-        )
+        );
       }
-      setNotificationsTransactions(lNotificationsTransactions)
+      setNotificationsTransactions(lNotificationsTransactions);
 
-      const lNotificationsInvoices = []
+      const lNotificationsInvoices = [];
       for (const { notification, invoice } of invoices) {
         lNotificationsInvoices.push(
-          <NavDropdown.Item key={`notification_invoice_${invoice.id}`} onClick={() => setSeen(notification)}>
-            {t('common.imported')}  {invoice.description}
-            <small> <NumberFormat t={t} value={invoice.total} /></small>
+          <NavDropdown.Item
+            key={`notification_invoice_${invoice.id}`}
+            onClick={() => setSeen(notification)}
+          >
+            {t("common.imported")} {invoice.description}
+            <small>
+              {" "}
+              <NumberFormat t={t} value={invoice.total} />
+            </small>
           </NavDropdown.Item>
-        )
+        );
       }
-      setNotificationsInvoices(lNotificationsInvoices)
-      const lNotificationsCaptchas = []
+      setNotificationsInvoices(lNotificationsInvoices);
+      const lNotificationsCaptchas = [];
       for (const captcha of captchas) {
         lNotificationsCaptchas.push(
-          <NavDropdown.Item className="captcha" key={`notification_captcha_${captcha.id}`} >
-            <img src={captcha.base64_url} alt={captcha.id} onClick={() => { confirmCaptcha(captcha.id) }} />
+          <NavDropdown.Item
+            className="captcha"
+            key={`notification_captcha_${captcha.id}`}
+          >
+            <img
+              src={captcha.base64_url}
+              alt={captcha.id}
+              onClick={() => {
+                confirmCaptcha(captcha.id);
+              }}
+            />
           </NavDropdown.Item>
-        )
+        );
       }
-      setNotificationsCaptcha(lNotificationsCaptchas)
-    }
-  }
+      setNotificationsCaptcha(lNotificationsCaptchas);
+    },
+  };
 
   function getConfig(id, defaultt, main_user = user) {
     let value = defaultt;
-    const valueConfig = main_user.configs ? main_user.configs.filter(({ config }) => config.id === id)[0] : null
+    const valueConfig = main_user.configs
+      ? main_user.configs.filter(({ config }) => config.id === id)[0]
+      : null;
     if (valueConfig) {
-      value = valueConfig.value
+      value = valueConfig.value;
     }
-    return value
+    return value;
   }
 
-  const lThemes = []
+  const lThemes = [];
   for (const themee of themes) {
     lThemes.push(
-      <NavDropdown.Item key={`theme_${themee}`} onClick={() => toogleTheme(themee)}>
+      <NavDropdown.Item
+        key={`theme_${themee}`}
+        onClick={() => toogleTheme(themee)}
+      >
         {themee}
       </NavDropdown.Item>
-    )
+    );
   }
   return (
     <div className="App">
-      <link rel="stylesheet" href={`https://cdn.jsdelivr.net/npm/bootswatch@5.0.1/dist/${theme}/bootstrap.min.css`}></link>
+      <link
+        rel="stylesheet"
+        href={`https://cdn.jsdelivr.net/npm/bootswatch@5.0.1/dist/${theme}/bootstrap.min.css`}
+      ></link>
       <style>{`\
         html{\
           font-size: ${fontSize * 12}px !important;\
         }\
-        ${compactMode ? '.hide-compact {display: none;}' : ''}
+        ${compactMode ? ".hide-compact {display: none;}" : ""}
       `}</style>
       <Navbar bg="light" expand="lg">
         <Container fluid>
@@ -170,17 +225,37 @@ function App({ t }) {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end" style={{ width: "100%" }}>
               <Nav.Link href="#compact" onClick={() => toogleCompactMode()}>
-                {
-                  compactMode ?
-                    <font className="text-success"><FontAwesomeIcon icon={faCompactDisc} /></font> :
-                    <font><FontAwesomeIcon icon={faCompactDisc} /></font>
-                }
+                {compactMode ? (
+                  <font className="text-success">
+                    <FontAwesomeIcon icon={faCompactDisc} />
+                  </font>
+                ) : (
+                  <font>
+                    <FontAwesomeIcon icon={faCompactDisc} />
+                  </font>
+                )}
               </Nav.Link>
               <Nav.Link href="#compact" onClick={() => toogleFontSize(0.9)}>
-                {fontSize < 1 ? <Badge pill className="bg-success">A-</Badge > : <Badge pill className="bg-secondary">A-</Badge >}
+                {fontSize < 1 ? (
+                  <Badge pill className="bg-success">
+                    A-
+                  </Badge>
+                ) : (
+                  <Badge pill className="bg-secondary">
+                    A-
+                  </Badge>
+                )}
               </Nav.Link>
               <Nav.Link href="#compact" onClick={() => toogleFontSize(1.1)}>
-                {fontSize > 1 ? <Badge pill className="bg-success">A+</Badge > : <Badge pill className="bg-secondary">A+</Badge >}
+                {fontSize > 1 ? (
+                  <Badge pill className="bg-success">
+                    A+
+                  </Badge>
+                ) : (
+                  <Badge pill className="bg-secondary">
+                    A+
+                  </Badge>
+                )}
               </Nav.Link>
               <NavDropdown alignRight title={theme} id="themes">
                 {lThemes}
@@ -188,11 +263,15 @@ function App({ t }) {
               <Nav.Link href="#state">{stateConnection}</Nav.Link>
               <div className="notifications">
                 <small className="count">{notificationCount}</small>
-                <NavDropdown alignRight title={<FontAwesomeIcon icon={faBell} />} id="notifications">
+                <NavDropdown
+                  alignRight
+                  title={<FontAwesomeIcon icon={faBell} />}
+                  id="notifications"
+                >
                   {notificationsTransactions}
-                  {notificationsInvoices ? <NavDropdown.Divider /> : ''}
+                  {notificationsInvoices ? <NavDropdown.Divider /> : ""}
                   {notificationsInvoices}
-                  {notificationsCaptcha ? <NavDropdown.Divider /> : ''}
+                  {notificationsCaptcha ? <NavDropdown.Divider /> : ""}
                   {notificationsCaptcha}
                 </NavDropdown>
               </div>
@@ -212,4 +291,4 @@ function App({ t }) {
   );
 }
 
-export default withNamespaces()(App);
+export default withTranslation()(App);
--- src/components/WebSocketComponent.js
+++ src/components/WebSocketComponent.js
@@ -5,121 +5,120 @@ import AccountModal from "./WebSocketComponent/modals/AccountModal";
 import AccountsTable from "./WebSocketComponent/AccountsTable";
 import InvoicesModal from "./WebSocketComponent/modals/InvoicesModal";
 import MessageReceiverEnum from "../enums/MessageReceiverEnum";
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React, { useState, useMemo, useRef, useEffect } from "react";
 import TransactionModal from "./WebSocketComponent/modals/TransactionModal";
 import TransactionsModal from "./WebSocketComponent/modals/TransactionsModal";
-import useWebSocket from 'react-use-websocket';
+import useWebSocket from "react-use-websocket";
 import YearTab from "./WebSocketComponent/YearTab";
 import InvoiceModal from "./WebSocketComponent/modals/InvoiceModal";
 import RepeatTransactionModal from "./WebSocketComponent/modals/RepeatTransactionModal";
 
+const SOCKET_URL = "ws://localhost:8765/";
+const PATH = btoa(
+  "guilherme.fabrin@gmail.com:$2a$12$ay271FTOwg4G2/ZUMZM17OZY6aRY4BZIpid1YJFiC26ZtyUZOxiVe"
+);
 
-const SOCKET_URL = 'ws://localhost:8765/'
-const PATH = btoa('guilherme.fabrin@gmail.com:$2y$10$8SIHjbAwDS/Cy4fVWwoPf.FM19.KrHAPrUrdWOp8ZGQdwLD/7Bxc2')
-
-let mainAccounts = []
-let mainSendJsonMessage = null
+let mainAccounts = [];
+let mainSendJsonMessage = null;
 
 const WebSocketComponent = ({ t, setters }) => {
-    const [socketUrl, setSocketUrl] = useState(SOCKET_URL + PATH);
-    const messageHistory = useRef([]);
+  const [socketUrl, setSocketUrl] = useState(SOCKET_URL + PATH);
+  const messageHistory = useRef([]);
 
-    const {
-        sendJsonMessage,
-        lastMessage,
-        readyState,
-    } = useWebSocket(socketUrl, {
-        onOpen: () => {
-            YearTab.change(new Date().getFullYear())
-        },
-        onMessage: (message) => {
-            const json = JSON.parse(message.data)
-            RECEIVERS[json.code](json)
-        },
-        shouldReconnect: (closeEvent) => true,
-    });
-    mainSendJsonMessage = sendJsonMessage
-    const RECEIVERS = Object.freeze({
-        [MessageReceiverEnum.USER]: ({ user }) => {
-            setters.user(user)
-        },
-        [MessageReceiverEnum.ACCOUNTS]: ({ accounts }) => {
-            mainAccounts = accounts
-            AccountsTable.update(accounts)
-        },
-        [MessageReceiverEnum.YEAR]: ({ year }) => {
-            YearTab.update(year)
-            AccountsTable.loading()
-        },
-        [MessageReceiverEnum.NOTIFICATIONS]: (data) => {
-            setters.notifications(data)
-        },
-        [MessageReceiverEnum.AUTOMATED]: ({ account_id, status }) => {
-            for (const index in mainAccounts) {
-                const account = mainAccounts[index]
-                if (account.id === account_id) {
-                    account.sync = status
-                }
-                mainAccounts[index] = account
-            }
-            AccountsTable.update(mainAccounts)
-        },
-        [MessageReceiverEnum.TRANSACTIONS]: ({ transactions }) => {
-            TransactionsModal.update(transactions)
-        },
-        [MessageReceiverEnum.INVOICES]: ({ invoices }) => {
-            InvoicesModal.update(invoices)
-        },
-        [MessageReceiverEnum.UPDATE]: () => {
-            YearTab.reload()
-            TransactionsModal.reload()
-            InvoicesModal.reload()
+  const { sendJsonMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => {
+      YearTab.change(new Date().getFullYear());
+    },
+    onMessage: (message) => {
+      const json = JSON.parse(message.data);
+      RECEIVERS[json.code](json);
+    },
+    shouldReconnect: (closeEvent) => true,
+  });
+  mainSendJsonMessage = sendJsonMessage;
+  const RECEIVERS = Object.freeze({
+    [MessageReceiverEnum.USER]: ({ user }) => {
+      setters.user(user);
+    },
+    [MessageReceiverEnum.ACCOUNTS]: ({ accounts }) => {
+      mainAccounts = accounts;
+      AccountsTable.update(accounts);
+    },
+    [MessageReceiverEnum.YEAR]: ({ year }) => {
+      YearTab.update(year);
+      AccountsTable.loading();
+    },
+    [MessageReceiverEnum.NOTIFICATIONS]: (data) => {
+      setters.notifications(data);
+    },
+    [MessageReceiverEnum.AUTOMATED]: ({ account_id, status }) => {
+      for (const index in mainAccounts) {
+        const account = mainAccounts[index];
+        if (account.id === account_id) {
+          account.sync = status;
         }
-    })
-    messageHistory.current = useMemo(() => {
-        return messageHistory.current.concat(lastMessage)
-    }, [lastMessage]);
-    useEffect(() => {
-        setSocketUrl(SOCKET_URL + PATH)
-    }, [])
-    setters.readyState(readyState)
-    return (
-        <div>
-            <h2>
-                {t('accounts.title')}
-            </h2>
-            <AccountModal.Elem t={t} accounts={mainAccounts} sendJsonMessage={sendJsonMessage} />
-            <TransactionModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <TransactionsModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <InvoicesModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <InvoiceModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <YearTab.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <AccountsTable.Elem t={t} sendJsonMessage={sendJsonMessage} />
-            <RepeatTransactionModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
-        </div>
-    );
+        mainAccounts[index] = account;
+      }
+      AccountsTable.update(mainAccounts);
+    },
+    [MessageReceiverEnum.TRANSACTIONS]: ({ transactions }) => {
+      TransactionsModal.update(transactions);
+    },
+    [MessageReceiverEnum.INVOICES]: ({ invoices }) => {
+      InvoicesModal.update(invoices);
+    },
+    [MessageReceiverEnum.UPDATE]: () => {
+      YearTab.reload();
+      TransactionsModal.reload();
+      InvoicesModal.reload();
+    },
+  });
+  messageHistory.current = useMemo(() => {
+    return messageHistory.current.concat(lastMessage);
+  }, [lastMessage]);
+  useEffect(() => {
+    setSocketUrl(SOCKET_URL + PATH);
+  }, []);
+  setters.readyState(readyState);
+  return (
+    <div>
+      <h2>{t("accounts.title")}</h2>
+      <AccountModal.Elem
+        t={t}
+        accounts={mainAccounts}
+        sendJsonMessage={sendJsonMessage}
+      />
+      <TransactionModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <TransactionsModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <InvoicesModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <InvoiceModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <YearTab.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <AccountsTable.Elem t={t} sendJsonMessage={sendJsonMessage} />
+      <RepeatTransactionModal.Elem t={t} sendJsonMessage={sendJsonMessage} />
+    </div>
+  );
 };
 
 export default {
-    Elem: WebSocketComponent,
-    setCaptcha: (id, value) => {
-        mainSendJsonMessage({
-            code: MessageReceiverEnum.CAPTCHA,
-            value,
-            id
-        });
-    },
-    setConfig: (id, value) => {
-        mainSendJsonMessage({
-            code: MessageReceiverEnum.CONFIG,
-            value,
-            id
-        });
-    },
-    setSeen: (id) => {
-        mainSendJsonMessage({
-            code: MessageReceiverEnum.NOTIFICATION,
-            id
-        });
-    }
-}
+  Elem: WebSocketComponent,
+  setCaptcha: (id, value) => {
+    mainSendJsonMessage({
+      code: MessageReceiverEnum.CAPTCHA,
+      value,
+      id,
+    });
+  },
+  setConfig: (id, value) => {
+    mainSendJsonMessage({
+      code: MessageReceiverEnum.CONFIG,
+      value,
+      id,
+    });
+  },
+  setSeen: (id) => {
+    mainSendJsonMessage({
+      code: MessageReceiverEnum.NOTIFICATION,
+      id,
+    });
+  },
+};
